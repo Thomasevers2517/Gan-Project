2a77e06193c32b775b4221b3e9cc2c858b8760c8
Anjali Anand <99anjali.anand@gmail.com>
2024-04-07 18:54:24 +0200

    Working on the scatter plot

--- visualizing model.py
+++ visualizing model.py
@@ -9,11 +9,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 
-Z_DIM = 100
-G_HIDDEN = 64
-D_HIDDEN = 64
-IMAGE_CHANNEL = 1
-IMAGE_SIZE = 784
+# Z_DIM = 100
+# G_HIDDEN = 64
+# D_HIDDEN = 64
+# IMAGE_CHANNEL = 1
+# IMAGE_SIZE = 784
 
 # Create an instance of the Generator and Discriminator
 # generator = Generator(Z_DIM, G_HIDDEN, IMAGE_CHANNEL)
@@ -61,26 +61,26 @@ IMAGE_SIZE = 784
 
 # generating plots for iterations
 
-iter_data = json.load(open('iter_info.json'))
-iter_last = json.load(open('iter_last.json'))
-z_dim = list(iter_data.keys())
-print(iter_data['1000']['10'][-1])
-m_dim = list(iter_data["10"].keys())
-print(m_dim)
-# plt.plot(iter_data['1000']['480'], label='M=480')
-# plt.show()
-iter_loss={}
-for z in z_dim:
-    iter_loss[z]={}
-    for m in m_dim:
-        iter_loss[z][m]=iter_data[z][m][-1]
-        plt.plot(iter_data[z][m], label='M='+m)
-        #plt.axvline(x=iter_last[z][m], color='r', linestyle='--', label='Early Stopping')
-        plt.xlabel("Iterations")
-        plt.ylabel("Loss")
-        plt.title(f"Loss for Z={z}")
-        plt.legend()
-    plt.show()
+# iter_data = json.load(open('iter_info.json'))
+# iter_last = json.load(open('iter_last.json'))
+# z_dim = list(iter_data.keys())
+# print(iter_data['1000']['10'][-1])
+# m_dim = list(iter_data["10"].keys())
+# print(m_dim)
+# # plt.plot(iter_data['1000']['480'], label='M=480')
+# # plt.show()
+# iter_loss={}
+# for z in z_dim:
+#     iter_loss[z]={}
+#     for m in m_dim:
+#         iter_loss[z][m]=iter_data[z][m][-1]
+#         plt.plot((np.array(iter_data[z][m])/int(m))/int(z), label='M='+m)
+#         #plt.axvline(x=iter_last[z][m], color='r', linestyle='--', label='Early Stopping')
+#         plt.xlabel("Iterations")
+#         plt.ylabel("Loss")
+#         plt.title(f"Loss for Z={z}")
+#         plt.legend()
+#     plt.show()
 
 # mse = pd.DataFrame.from_dict(iter_loss)
 # mse = pd.DataFrame(mse, index=m_dim)
@@ -112,4 +112,39 @@ for z in z_dim:
 # plt.show()
 
 
+iter_last = json.load(open('iter_last.json'))
+loss_data = json.load(open('compression_MSE_onlyepoch16.json'))
+#os.mkdir(save_path)
 
+z_dim = list(loss_data.keys())
+print(z_dim)
+epochs = list(loss_data["1000"].keys())
+print(epochs)
+m_dim = list(loss_data["1000"]["16"].keys())
+print(m_dim)
+alphas = list(loss_data["1000"]["16"]["480"].keys())
+print(alphas)   
+epoch='16'
+alpha='0.04'
+MSE = pd.DataFrame(columns = ['Z', 'M', 'Last_iter', 'MSE'])
+for z in z_dim:
+    for m in m_dim:
+        df= {'Z': z, 'M': m, 'Last_iter': iter_last[z][m], 'MSE': np.mean(loss_data[z][epoch][m][alpha])} 
+        MSE = pd.concat([MSE, pd.DataFrame([df])], ignore_index=True)
+
+
+colour= {'10': 'pink', '50': 'blue', '75': 'green', '100': 'orange', '150': 'red', '200': 'skyblue', '1000': 'brown'}
+marker={'10': 'o', '40': '^', '160': 'D', '480': 'P'}
+fig, ax = plt.subplots()
+for i in range(len(MSE)):
+    ax.scatter(MSE['Last_iter'][i], MSE["MSE"][i], c=colour[MSE["Z"][i]], marker=marker[MSE["M"][i]])
+
+markers1 = [plt.Line2D([0,0],[0,0],color=color, marker='o', linestyle='') for color in colour.values()]
+firstlegend= ax.legend(markers1, colour.keys(), numpoints=1, title="Z Dimension", loc='upper right',bbox_to_anchor=(1.05, 1))
+ax.add_artist(firstlegend)
+markers2 = [plt.Line2D([0,0],[0,0],color='black', marker=marker, linestyle='') for marker in marker.values()]
+ax.legend(markers2, marker.keys(), numpoints=1, title="M Dimension", loc='lower right', bbox_to_anchor=(1.05, 0))
+plt.xlabel("Iterations")
+plt.ylabel("MSE")
+plt.title("Iterations vs MSE")
+plt.show()
